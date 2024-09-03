a0532f98ca87992261c02d5787957cc0100c038c
Sarun Kaewwongsa <k.sarun@odds.team>
2024-09-03 22:58:41 +0700

    updated userList

--- cypress/e2e/saucedemo-test.cy.ts
+++ cypress/e2e/saucedemo-test.cy.ts
@@ -13,18 +13,6 @@ describe('Sauce demo', () => {
         {userName: "RubeusHagrid", password: "password6"},
         {userName: "DracoMalfoy", password: "password7"},
         {userName: "SiriusBlack", password: "password8"},
-        {userName: "RemusLupin", password: "password9"},
-        {userName: "MinervaMcGonagall", password: "password10"},
-        {userName: "GellertGrindelwald", password: "password11"},
-        {userName: "Voldemort", password: "password12"},
-        {userName: "LunaLovegood", password: "password13"},
-        {userName: "NevilleLongbottom", password: "password14"},
-        {userName: "GinnyWeasley", password: "password15"},
-        {userName: "FredWeasley", password: "password16"},
-        {userName: "GeorgeWeasley", password: "password17"},
-        {userName: "CedricDiggory", password: "password18"},
-        {userName: "ChoChang", password: "password19"},
-        {userName: "NymphadoraTonks", password: "password20"}
     ];
 
     const login = new LoginPage();
@@ -41,7 +29,7 @@ describe('Sauce demo', () => {
     userList.forEach((value) => {
         it(`login failed with user -> ${value.userName}`, () => {
 
-        login.InvalidUser(value.userName, value.password)
+        login.InvalidUser(value.userName, value.password);
 
         });
     });
